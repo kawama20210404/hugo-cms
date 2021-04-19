ef2241d810df3546925b1d4bd9fd3e68e8b34600
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-04-19 17:32:48 +0900

    no message

--- functions/validate.js
+++ functions/validate.js
@@ -1,5 +1,11 @@
 //NG ワード
 const words = ["シメサバ"]
+
+
+const headers = {
+"Content-Type":"application/json;charset=UTF-8"
+}
+
 exports.handler = function({httpMethod,queryStringParameters},context,callback)
 {
 
