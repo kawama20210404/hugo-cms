3978dce4a1620d5b2338277c9ed18f7b19dd608a
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-04-19 15:33:09 +0900

    function実装

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,6 @@
-exports.handler = (event,context,callback) => {
-	callback(null,{
-		statusCode:200,
-		body:"Hello World!",
-	})
-
+exports.handler = function(event, context, callback) {
+	callback(null, {
+	statusCode: 200,
+	body: 'Hello World'
+	});
 }
