3f5eccd8368cca880f7255f7512d034b62b3f144
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-04-19 16:48:57 +0900

    no message

--- functions/validate.js
+++ functions/validate.js
@@ -10,14 +10,10 @@ exports.handler = ({httpMethod,queryStringParameters},context,callback)
 	const { word } = queryStringParameters
 
 	if(words.includes(word){
-		callback(null,{
-			statusCode:401,
-			body:JSON.stringify({
-				message:'${word}には不適切な単語が含まれています',
-			}),
-			headers,
-		})
-		return
+	callback(null, {
+	statusCode: 401,
+	body: '不適切な単語が含まれています'
+	});
 	}
 
 
