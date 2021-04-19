d6e90762332cff2d86c7226a64f98b888dafde03
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-04-19 17:29:17 +0900

    no message

--- functions/validate.js
+++ functions/validate.js
@@ -7,14 +7,5 @@ const headers={"Content-Type":"application/json;charset=UTF-8"}
 exports.handler = function({httpMethod,queryStringParameters},context,callback)
 {
 
-	const { word } = queryStringParameters
-
-	if(words.includes(word){
-	callback(null, {
-	statusCode: 401,
-	body: '•s“KØ‚È’PŒê‚ªŠÜ‚Ü‚ê‚Ä‚¢‚Ü‚·'
-	});
-	}
-
 
 }
