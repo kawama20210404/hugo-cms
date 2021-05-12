e9640cdbe069febd327a5d017f85fd54e38cd4ed
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-12 16:28:43 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -3,11 +3,9 @@
 		statusCode: 200,
 		body: 'Hello World3',
 		});
-		
+	}	
+
 				<img 
   		src="http://52.192.18.116/Image001.png?nf_resize=fit&w=1000"
   		alt="Screenshots of CSS-Tricks and CodePen homepages"
 		/>
-
-	}	
-
