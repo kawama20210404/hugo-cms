5b223d15a2c0e17e775a7b7ce1acd4bd71403855
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-12 15:27:16 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -1,7 +1,8 @@
 	exports.handler = function(event, context, callback) {	
 		callback(null, {
 		statusCode: 200,
-		body: 'Hello World',image: "http://54.199.242.54/Image001.png"
+		body: 'Hello World',
+		image: "http://54.199.242.54/Image001.png"
 		});
 
 	}	
