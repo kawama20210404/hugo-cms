8cb31f1b93092db639e337162b8f000aeb6ced54
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-12 16:26:24 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -1,13 +1,13 @@
 	exports.handler = function(event, context, callback) {	
 		callback(null, {
 		statusCode: 200,
-		body: 'Hello World2',
-		<img 
+		body: 'Hello World3',
+		});
+		
+				<img 
   		src="http://52.192.18.116/Image001.png?nf_resize=fit&w=1000"
   		alt="Screenshots of CSS-Tricks and CodePen homepages"
 		/>
-		});
-		
 
 	}	
 
