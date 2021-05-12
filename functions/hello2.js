ba561191d861f46451c07e93b7dd44ab2e2fcf25
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-12 15:09:23 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -2,6 +2,6 @@
 		callback(null, {
 		statusCode: 200,
 		body: 'Hello World'
-		<img src="http://54.199.242.54/Image001.png" class="favicon">
 		});
+				<img src="http://54.199.242.54/Image001.png" alt="" class="star-count">
 	}	
