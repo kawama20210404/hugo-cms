0756342175de54e541af99c9dca1d02bb0677a08
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-12 16:24:24 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -1,18 +1,13 @@
 	exports.handler = function(event, context, callback) {	
 		callback(null, {
 		statusCode: 200,
-		body: 'Hello World2'
+		body: 'Hello World2',
+		<img 
+  		src="http://52.192.18.116/Image001.png?nf_resize=fit&w=1000"
+  		alt="Screenshots of CSS-Tricks and CodePen homepages"
+		/>
 		});
 		
-		
-		const handler = StripeCheckout.configure({
-  		key: STRIPE_PUBLISHABLE_KEY,
-  		image: "http://52.192.18.116/Image001.png",
-  		locale: "auto",
-  		token: token => {
-    		// We'll fill this out in a second.
-  		}
-		});
 
 	}	
-	
+
