edfc9202ccdd36697b0d3e75ffacf31ab4d2c1cf
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-12 18:17:59 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -1,20 +1,23 @@
-	exports.handler = function(event, context, callback) {	
-		callback(null, {
-		statusCode: 200,
-		body: 'Hello World3',
-		});
-	}	
+// functions/newsletter.js
+exports.handler = async (event) => {
+  const { body } = event; // Request body data
 
-import Image from 'next/image';
+  // Basic example of sending the data to an external API
+  fetch(`http://52.192.18.116/Image001.png`, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      // Add credentials as required by the external service
+    },
+    body: body, // Send the data
+  })
+    .then((response) => {
+      // Do stuff and returns 200 response...
+    })
+    .catch((error) => {
+      // Do stuff and returns 4xx / 5xx response...
+    });
+};
 
-export default function MyImage() {
-  return (
-    <Image
-      src="http://52.192.18.116/Image001.png"
-      alt="what a great boop"
-      width="1368"
-      height="1044"
-      layout="responsive"
-    />
-  );
-}}
+// POST localhost:8888/.netlify/functions/newsletter
+// → 200 OK
