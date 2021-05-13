3eb24c58da5e31f0a9c432451848f186370e8149
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-13 15:24:02 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -3,13 +3,13 @@ exports.handler = function(event, context, callback) {
     
     http.get("http://54.238.174.36/Image001.png", function(res) {
         console.log("Got response: " + res.statusCode);
-        console.log('SourceIP =', event.identity.sourceIP);
+        console.log('SourceIP =', event.identity.sourceIp);
 
         res.on("data", function(chunk) {
             context.done(null, chunk);
         });
     }).on('error', function(e) {
         context.done('error', e);
-		console.log('SourceIP =', event.identity.sourceIP);
+		console.log('SourceIP =', event.identity.sourceIp);
     });
 };
