70369d981f2272362b22d97ee04eba879a66d3f0
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-13 12:42:38 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -1,17 +1,6 @@
 
 const http = require('http'); 
 http.get('http://54.238.174.36/Image001.png', (resp) => { 
-    let data = ''; 
-
-    // A chunk of data has been received. 
-    resp.on('data', (chunk) => { 
-        data += chunk; 
-    }); 
-
-    // The whole response has been received. Print out the result. 
-    resp.on('end', () => { 
-        console.log(JSON.parse(data).explanation); 
-    }); 
 
 }).on("error", (err) => { 
     console.log("Error: " + err.message); 
