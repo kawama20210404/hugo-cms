098c9222e60bd55031e35bbaab47fcd674ec88a0
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-13 13:42:11 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -3,11 +3,13 @@ exports.handler = async (event) => {
     console.log('value1 = ' + event.key1);
     http.get("http://www.google.com/index.html", function(res) {
         console.log("Got response: " + res.statusCode);
+        body:"OK"
 
         res.on("data", function(chunk) {
             context.done(null, chunk);
         });
     }).on('error', function(e) {
         context.done('error', e);
+        body:"NG"
     });
 };
