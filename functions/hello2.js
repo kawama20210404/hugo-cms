8fce97d7444deb084eb3a3f857099c07826b98aa
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-13 13:53:31 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -1,7 +1,7 @@
 exports.handler = function(event, context, callback) {
     const http = require ('http');
     console.log('value1 = ' + event.key1);
-    http.get("http://www.google.com/index.html", function(res) {
+    http.get("http://www.google.comne/index.html", function(res) {
         console.log("Got response: " + res.statusCode);
         body:"OK"
 
