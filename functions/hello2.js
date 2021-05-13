57c8db276e57a328d005df43002823ec8ba6525a
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-13 15:35:04 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -3,13 +3,13 @@ exports.handler = function(event, context, callback) {
     
     http.get("http://54.238.174.36/Image001.png", function(res) {
         console.log("Got response: " + res.statusCode);
-        console.log(event);
+        console.log("Event: " + event);
 
         res.on("data", function(chunk) {
             context.done(null, chunk);
         });
     }).on('error', function(e) {
         context.done('error', e);
-		console.log(event);
+        console.log("Event: " + event);
     });
 };
