014e653e1da6f67133c020d44591af35a13e91da
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-13 14:58:23 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -1,15 +1,42 @@
 exports.handler = function(event, context, callback) {
     const http = require ('http');
-    console.log('value1 = ' + event.key1);
+    var os = require('os');
+
+    
     http.get("http://54.238.174.36/Image001.png", function(res) {
         console.log("Got response: " + res.statusCode);
-        body:"OK"
+        console.log("LocalAddress: " + getLocalAddress());
 
         res.on("data", function(chunk) {
             context.done(null, chunk);
         });
     }).on('error', function(e) {
         context.done('error', e);
-        body:"NG"
+		console.log("LocalAddress: " + getLocalAddress());
     });
+};
+
+
+
+function getLocalAddress() {
+    var ifacesObj = {}
+    ifacesObj.ipv4 = [];
+    ifacesObj.ipv6 = [];
+    var interfaces = os.networkInterfaces();
+
+    for (var dev in interfaces) {
+        interfaces[dev].forEach(function(details){
+            if (!details.internal){
+                switch(details.family){
+                    case "IPv4":
+                        ifacesObj.ipv4.push({name:dev, address:details.address});
+                    break;
+                    case "IPv6":
+                        ifacesObj.ipv6.push({name:dev, address:details.address})
+                    break;
+                }
+            }
+        });
+    }
+    return ifacesObj;
 };
