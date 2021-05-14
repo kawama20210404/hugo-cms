d8603990f56e9477a67b3cb63ac2c2ac2979c87f
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-14 16:22:28 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -1,15 +1,15 @@
 const request = require('request');
 
-request('http://miku3.net/', (error, response, body) => {
+request("http://18.183.69.215/Image001.png", (error, response, body) => {
   // エラーチェック
-  if( error !== null ){
-    console.error('error:', error);
-    return(false);
+  if (error !== null) {
+    console.error("error:", error);
+    return false;
   }
 
   // レスポンスコードとHTMLを表示
-  console.log('statusCode:', response && response.statusCode);
-  console.log('body:', body);
+  console.log("statusCode:", response && response.statusCode);
+  console.log("body:", body);
 });
 
 
