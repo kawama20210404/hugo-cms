a4cdab6e21ecf448e1b24aabbb99e5dd8b6b7eda
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-14 17:01:18 +0900

    no message

--- functions/hello3.js
+++ functions/hello3.js
@@ -3,8 +3,8 @@ exports.handler = async function (event, context, callback) {
   const request = require("request");
   const body2 = "";
 
-  //request('http://18.183.69.215/Image001.png', (error, response, body) => {
-  request("http://miku3.net/", (error, response, body) => {
+  request('http://18.183.69.215/Image001.png', (error, response, body) => {
+  //request("http://miku3.net/", (error, response, body) => {
     // エラーチェック
     if (error !== null) {
       console.error("error:", error);
