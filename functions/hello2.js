6e6b0a1bebbd71bc23740f95daff111624805d1f
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-14 16:10:47 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -2,7 +2,8 @@
 exports.handler = async function(event, context) {
     const request = require('request');
 
-request('http://18.183.69.215/Image001.png', (error, response, body) => {
+//request('http://18.183.69.215/Image001.png', (error, response, body) => {
+request('http://miku3.net/', (error, response, body) => {
   // エラーチェック
   if( error !== null ){
     console.error('error:', error);
