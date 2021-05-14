88d9f74f4247d762558cb84c1531faa792927efd
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-14 16:32:29 +0900

    no message

--- functions/hello3.js
+++ functions/hello3.js
@@ -1,28 +1,24 @@
 //exports.handler = function(event, context, callback) {
-exports.handler = async function(event, context) {
-    const request = require('request');
+exports.handler = async function (event, context, callback) {
+  const request = require("request");
 
-//request('http://18.183.69.215/Image001.png', (error, response, body) => {
-request('http://miku3.net/', (error, response, body) => {
-  // エラーチェック
-  if( error !== null ){
-    console.error('error:', error);
-    return(false);
-  }
+  //request('http://18.183.69.215/Image001.png', (error, response, body) => {
+  request("http://miku3.net/", (error, response, body) => {
+    // エラーチェック
+    if (error !== null) {
+      console.error("error:", error);
+      return false;
+    }
 
-  // レスポンスコードとHTMLを表示
-  console.log('statusCode:', response && response.statusCode);
-  console.log('body:', body);
-});
+    // レスポンスコードとHTMLを表示
+    console.log("statusCode:", response && response.statusCode);
+    console.log("body:", body);
+  });
 
-
-    callback(null, {
-      //body:$event.identity.sourceIp
-      body: body,
-    });
-    
-
-    
+  callback(null, {
+    //body:$event.identity.sourceIp
+    body: body,
+  });
 };
 
 
