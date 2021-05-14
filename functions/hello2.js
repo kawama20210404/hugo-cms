d1b650ab55bbebcb1630446c332aba1ee3531efe
kawama20210404 <kawama.kiyoshi@ricksoft.jp>
2021-05-14 15:01:49 +0900

    no message

--- functions/hello2.js
+++ functions/hello2.js
@@ -1,24 +1,18 @@
 exports.handler = function(event, context, callback) {
-    const http = require ('http');
-    
-    http.get("http://18.183.69.215/Image001.png", function(res) {
-        console.log("Got response: " + res.statusCode);
-        console.log("Event: " + event);
+    const request = require('request');
 
-        res.on("data", function(chunk) {
-            context.done(null, chunk);
-        });
-    }).on('error', function(e) {
-        context.done('error', e);
-        console.log("Event: " + event);
-    });
-    
-    
-    
-    callback(null,{
-    	//body:$event.identity.sourceIp
-    	body:JSON.stringify(event)
-	})
-    
+request('http://18.183.69.215/Image001.png', (error, response, body) => {
+  // エラーチェック
+  if( error !== null ){
+    console.error('error:', error);
+    return(false);
+  }
+
+  // レスポンスコードとHTMLを表示
+  console.log('statusCode:', response && response.statusCode);
+  console.log('body:', body);
+});
     
 };
+
+
